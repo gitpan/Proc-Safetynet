e33401883b500c25bc0537a418dcdfb60f11adaf
Dexter Tad-y <dtady@cpan.org>
2008-12-08 11:20:33 +0000

    Import of DTADY/Proc-Safetynet-0.01 from CPAN.
    gitpan-cpan-distribution: Proc-Safetynet
    gitpan-cpan-version:      0.01
    gitpan-cpan-path:         DTADY/Proc-Safetynet-0.01.tar.gz
    gitpan-cpan-author:       DTADY
    gitpan-cpan-maturity:     released

---
+++ bin/safetynetd.pl
@@ -0,0 +1,57 @@
+#!/usr/bin/perl -wT
+use strict;
+use warnings;
+
+use Proc::Safetynet;
+use Proc::Safetynet::RpcServer::Unix;
+use Proc::Safetynet::Program::Storage::TextFile;
+
+use Fcntl ':flock';
+use Config::General;
+use Data::Dumper;
+
+my $usage = <<EOF;
+Usage: $0 <config_file>
+EOF
+
+my $lockfh;
+my $config;
+{
+    # validate config file
+    my $config_file = shift @ARGV || '';
+    (-e $config_file)
+        or die $usage;
+    # lock config file or die
+    open $lockfh, $config_file
+        or die "unable to open config file: $config_file: $!";
+    flock($lockfh, LOCK_EX|LOCK_NB)
+        or exit(2); # unable to lock
+    print STDERR "$$: started...\n"; # we have acquired the lock
+    my $rc = Config::General->new( $config_file );
+    $config = { $rc->getall() };
+}
+
+my $programs = Proc::Safetynet::Program::Storage::TextFile->new(
+    file        => $config->{programs},
+);
+$programs->reload;
+
+# ---------
+
+my $supervisor = Proc::Safetynet::Supervisor->spawn(
+    alias           => q{SUPERVISOR},
+    binpath         => $config->{binpath},
+    programs        => $programs,
+);
+if (exists $config->{unix_server}) {
+    Proc::Safetynet::RpcServer::Unix->spawn(
+        alias           => q{UNIXSERVER},
+        supervisor      => $supervisor->alias,
+        session_class   => 'Proc::Safetynet::RpcSession::Simple',
+        socket          => $config->{unix_server}->{socket},
+    );
+}
+
+POE::Kernel->run();
+
+__END__
